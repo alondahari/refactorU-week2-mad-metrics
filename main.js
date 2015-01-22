eb76a67277d8b973b1b6e08c5a375bb9d6f2cf00
Alon Dahari <climbinghobo@gmail.com>
2015-01-22 11:32:18 -0700

    add percent viewed

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 $(document).on('ready', function() {
-  
+
+	// Functions
+	var addLightboxLine = function(header, value){
+  	$lightboxContent.append('<p>' + header + ': ' + value + '</p>');
+	};
+
+	var setPrecentViewed = function(){
+		var current = Math.ceil((document.body.scrollTop + window.innerHeight) / $(document).height() * 100);
+		return (data.percent > current) ? data.percent : current;
+	};
+
+	// Globals
+	var data = {}
+	data.percent = setPrecentViewed();
+
+
+	var $lightboxContent = $('.lightbox-content');
+
+	// Events
+  $('.button').on('click', function() {
+		$lightboxContent.empty();
+  	$('.lightbox-screen').toggleClass('hidden');
+
+  	addLightboxLine('Percentage of page viewed', data.percent);
+
+  });
+
+  $(window).on('scroll', function() {
+  	data.percent = setPrecentViewed();
+  });
+
 });
