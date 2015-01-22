635e1ae117a2de4cc028cf03b857e146f63fdd1e
Alon Dahari <climbinghobo@gmail.com>
2015-01-22 11:41:41 -0700

    add total scroll

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ $(document).on('ready', function() {
   	$lightboxContent.append('<p>' + header + ': ' + value + '</p>');
 	};
 
-	var setPrecentViewed = function(){
+	var getPrecentViewed = function(){
 		var current = Math.ceil((document.body.scrollTop + window.innerHeight) / $(document).height() * 100);
 		return (data.percent > current) ? data.percent : current;
 	};
 
-	// Globals
-	var data = {}
-	data.percent = setPrecentViewed();
+	var getDistanceScrolled = function(){
+		if (data.currentScroll > document.body.scrollTop) {
+			return data.currentScroll - document.body.scrollTop;
+		} else {
+			return document.body.scrollTop - data.currentScroll;
+		}
+	};
 
+	// Globals
+	var data = {
+		scrolled: 0,
+		currentScroll: document.body.scrollTop
+	}
+	data.percent = getPrecentViewed();
 
 	var $lightboxContent = $('.lightbox-content');
 
@@ -23,11 +33,13 @@ $(document).on('ready', function() {
   	$('.lightbox-screen').toggleClass('hidden');
 
   	addLightboxLine('Percentage of page viewed', data.percent);
+  	addLightboxLine('Total get Distance Scrolled', data.scrolled + 'px');
 
   });
 
   $(window).on('scroll', function() {
-  	data.percent = setPrecentViewed();
+  	data.percent = getPrecentViewed();
+  	data.scrolled += getDistanceScrolled();
   });
 
 });
